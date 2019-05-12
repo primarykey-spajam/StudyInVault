269087d35e12ef7b9676b29dc6ba44c99a73b013
sk-mk2 <suke.badminton7@icloud.com>
2019-05-12 14:09:52 +0900

    :tada:Init Project

---
+++ ShareStoryBoard/ShareStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareStoryBoard
+//
+//  Created by 助永悠輝 on 2019/05/12.
+//  Copyright © 2019 助永悠輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
